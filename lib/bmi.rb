2c1dd24bbc7820034a6e4cf398c235edaacbaaad
Takayuki Kamiyama <karuma.reason@gmail.com>
2024-05-01 20:26:58 +0900

    Update.

--- lib/bmi.rb
+++ lib/bmi.rb
@@ -3,16 +3,13 @@
 lib = File.expand_path('lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
-require 'tanraku'
-
-# Encoding Setting.
-def encoding_style
-  Encoding.default_internal = 'UTF-8'
-  Encoding.default_external = 'UTF-8'
-end
-
 module BMI
   module_function
+
+  def encoding_style
+    Encoding.default_internal = Encoding::UTF_8
+    Encoding.default_external = Encoding::UTF_8
+  end
 
   def calc(kg, m)
     kg.to_f / (m.to_f ** 2)
@@ -20,28 +17,5 @@ module BMI
 end
 
 include BMI
-
-begin
-    encoding_style
-    fomula = calc(kg = ARGV[0], m = ARGV[1]).round(1)
-
-    if (fomula < 18.5)
-        puts '低体重' + " : #{fomula}"
-    elsif (fomula < 24.9)
-        puts '普通体重'+ " : #{fomula}"
-    elsif (fomula < 29.9)
-        puts '肥満(1度)'+ " : #{fomula}"
-    elsif (fomula < 34.9)
-        puts '肥満(2度)'+ " : #{fomula}"
-    elsif (fomula < 39.9)
-        puts '肥満(3度)'+ " : #{fomula}"
-    else
-        puts '肥満(4度)'+ " : #{fomula}"
-    end
-rescue Exception => e
-    puts e.backtrace.tanraku_exit
-ensure
-    GC.compact
-end
 
 __END__
