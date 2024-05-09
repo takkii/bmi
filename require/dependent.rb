5c573b1f723cadd43a51de1a7395fda09de0dbe3
Takayuki Kamiyama <karuma.reason@gmail.com>
2024-05-09 16:15:58 +0900

    Update.

---
+++ require/dependent.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# --------------------------------------
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/unit'
+require 'minitest/runner'
+require 'bmi'
+# --------------------------------------
