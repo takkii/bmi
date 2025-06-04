4c768d0d809a4905d2126b1480f642d0d802f83a
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-06-05 06:16:14 +0900

    Create.

---
+++ require/full_run.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# --------------------------------------
+# minitest / TDD.
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/unit'
+# --------------------------------------
+# dependency library other
+require 'rbconfig'
+# --------------------------------------
+# dependency library myself
+require 'bmi'
+require 'tanraku'
+# --------------------------------------
+# minitest-runner
+require 'minitest/full_runner'
+# --------------------------------------
+# Ignore rubygems.
+#require 'simplecov'
+# --------------------------------------
---
+++ runner/mini.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require "#{File.dirname(__FILE__)}/../require/mini_run"
+
+# The new instance will be deleted after process ends.
+class MiniTestFile
+  attr_reader :minitest
+
+  def initialize
+    encoding_style
+    host_os = RbConfig::CONFIG['host_os']
+    case host_os
+    when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+      begin
+        # Mini, Switch ON / (true, string).
+        @minitest = Mini(true, '/GitLab/bmi/minitest')
+      rescue
+        tanraku_execute
+      end
+    when /darwin|mac os/
+      # Don't have Macintosh PC.
+    when /linux/
+      begin
+        # MiniW, Switch ON / (true, string, string).
+        @minitest = MiniW(true, '/mnt/c/Users/sudok', '/GitLab/bmi/minitest')
+      rescue
+        tanraku_execute
+      end
+    else
+      begin
+        raise
+      rescue
+        puts 'Env, UNIX and Windows only. Other OS is Exception.'
+        tanraku_execute
+      end
+    end
+  end
+
+  def remove
+    remove_instance_variable(:@minitest)
+  end
+end
+
+# Coverage, Start 'SimpleCov.start' add.
+begin
+  MiniTestFile.new.remove
+rescue
+  puts 'Tanraku_VERSION: '.to_s + Tanraku::VERSION
+  tanraku_execute
+ensure
+  GC.compact
+end
+
+__END__
